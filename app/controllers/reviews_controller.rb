4e6de2ff881630157418f01ebb56b8681c811790
Raghu Betina <raghu@firstdraft.com>
2017-01-11 03:43:41 +0000

    Generate reviews resource with starter:resource

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,87 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+
+    render("reviews/index.html.erb")
+  end
+
+  def show
+    @review = Review.find(params[:id])
+
+    render("reviews/show.html.erb")
+  end
+
+  def new
+    @review = Review.new
+
+    render("reviews/new.html.erb")
+  end
+
+  def create
+    @review = Review.new
+
+    @review.user_id = params[:user_id]
+    @review.number_stars = params[:number_stars]
+    @review.content = params[:content]
+    @review.images = params[:images]
+    @review.restaurant_id = params[:restaurant_id]
+
+    save_status = @review.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/reviews/new", "/create_review"
+        redirect_to("/reviews")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Review created successfully.")
+      end
+    else
+      render("reviews/new.html.erb")
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+
+    render("reviews/edit.html.erb")
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    @review.user_id = params[:user_id]
+    @review.number_stars = params[:number_stars]
+    @review.content = params[:content]
+    @review.images = params[:images]
+    @review.restaurant_id = params[:restaurant_id]
+
+    save_status = @review.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/reviews/#{@review.id}/edit", "/update_review"
+        redirect_to("/reviews/#{@review.id}", :notice => "Review updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Review updated successfully.")
+      end
+    else
+      render("reviews/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+
+    @review.destroy
+
+    if URI(request.referer).path == "/reviews/#{@review.id}"
+      redirect_to("/", :notice => "Review deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Review deleted.")
+    end
+  end
+end
